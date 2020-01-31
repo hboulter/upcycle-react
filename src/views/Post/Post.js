bea31bcca46368c3d33f2d386235848db59e959f
Heidi Boulter <boulter.heidi@gmail.com>
2020-01-31 11:01:29 -0600

    post route

--- src/views/Post/Post.js
+++ src/views/Post/Post.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Map from "../../components/Map";
+import "./Post.css";
 
 const Post = () => {
   const googleMapUrl = `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_MAPS_KEY}`;
   return (
-    <Map
-      googleMapURL={googleMapUrl}
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-      isMarkerShown={true}
-    />
+    <div className="body">
+      <Map
+        googleMapURL={googleMapUrl}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        isMarkerShown={true}
+      />
+    </div>
   );
 };
 
